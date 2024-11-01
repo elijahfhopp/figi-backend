c1eceeca888e1e97c6b5c33e0fc4170888e533d4
Elijah Hopp <github.mrzeusthecoder@gmail.com>
2024-10-31 23:05:00 -0400

    Recognize CLI configed env.

--- figi/config.py
+++ figi/config.py
@@ -1,3 +1,4 @@
+import os
 from typing import Any, Dict
 
 from dotenv import dotenv_values
@@ -8,4 +9,5 @@ CONFIG: Dict[str, Any] = {
     "FIGI_MODEL_PATH": ".",
     "FIGI_INDEX": False,
     **dotenv_values(".env"),
+    **os.environ,
 }
