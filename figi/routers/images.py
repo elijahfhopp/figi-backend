26bd43a85a743445df09b1c9cb2521fa708524e9
Elijah Hopp <github.mrzeusthecoder@gmail.com>
2024-10-31 22:42:59 -0400

    Make images API only return JPEG.

--- figi/routers/images.py
+++ figi/routers/images.py
@@ -1,7 +1,7 @@
 import os
 
-from fastapi import APIRouter, HTTPException
-from fastapi.responses import FileResponse
+import cv2
+from fastapi import APIRouter, HTTPException, Response
 
 from figi.config import CONFIG
 from figi.db.models import ImagesModel
@@ -10,7 +10,7 @@ images = APIRouter()
 
 
 @images.get("/{image_id}")
-async def get_image(image_id: int) -> FileResponse:
+async def get_image(image_id: int) -> Response:
     print(image_id)
     image_model: ImagesModel = ImagesModel.get_or_none(ImagesModel.id == image_id)
     if not image_model:
@@ -21,4 +21,8 @@ async def get_image(image_id: int) -> FileResponse:
 
     root = CONFIG["FIGI_IMAGES_PATH"]
     path = os.path.join(root, str(image_model.path))
-    return FileResponse(path=path)
+    # Unless IMREAD_ANYDEPTH is set, it will load as uint8
+    image = cv2.imread(path)
+    encoded_image = cv2.imencode(".jpeg", image)[1].tobytes()
+
+    return Response(content=encoded_image, media_type="image/jpeg")
